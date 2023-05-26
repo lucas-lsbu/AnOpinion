63a4745561c12d649f65d7d6bfdba31796fc8c6b
Lucas Fernandes Roncolato <fernal13@lsbu.ac.uk>
2023-05-26 01:46:09 +0100

    added content with simple styling to the login/register page added the navigation route snapshot to show correct content

--- src/app/modules/auth/page/sign/sign.component.ts
+++ src/app/modules/auth/page/sign/sign.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign.component.css']
 })
 export class SignComponent {
+  isRegister?: boolean;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+
+
+  }
+  
+
+  // checking if it is currently on '/auth/register' or '/auth/login'
+  // to show login or register content
+  ngOnInit(): void {
+    this.isRegister = this.route.snapshot.routeConfig?.path == 'register' ? true : false;
+    console.log(this.isRegister)
+  }
 
 }
