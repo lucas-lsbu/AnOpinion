5e99e835895b925872c14f526af5a207912b6555
Lucas Fernandes Roncolato <fernal13@lsbu.ac.uk>
2023-06-01 18:39:58 +0100

    Added route guards Based on login condition added route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+import { AuthGuard } from "@angular/fire/auth-guard";
+
+const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo(['auth'])
+const redirectLoggedInToDashboard = () => redirectLoggedInTo(['dashboard']);
+
 const routes: Routes = [
   { 
     path: 'auth',
     component: AuthLayoutComponent,
-    loadChildren: () => import ('./modules/auth/auth.module').then(module => module.AuthModule)
+    loadChildren: () => import ('./modules/auth/auth.module').then(module => module.AuthModule),
+    canActivate: [AuthGuard], data: { authGuardPipe: redirectLoggedInToDashboard }
   },
   {
     path: 'dashboard',
     component: DashboardLayoutComponent,
-    loadChildren: () => import ('./modules/dashboard/dashboard.module').then(module => module.DashboardModule)
+    loadChildren: () => import ('./modules/dashboard/dashboard.module').then(module => module.DashboardModule),
+    canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToAuth }
   },
   { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
